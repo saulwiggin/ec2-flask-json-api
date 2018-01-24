05b12aa5ee6d714c42d2d37b61ebc3de16dbeafa
saulwiggin <saulwiggin@googlemail.com>
2018-01-24 09:40:35 +0000

    add seek-seq

--- src/application.py
+++ src/application.py
@@ -2,14 +2,20 @@
 from flask import Flask
 from flaskrun import flaskrun
 from flask_cors import CORS
+from seqseek import Chromosome
 
 application = Flask(__name__)
 CORS(application)
 
+@application.route('/json', methods=['GET'])
+def get():
+    print('Vitamin B6: rs4654748')
+    ref_chromo = Chromosome(1).sequence(21786068,21786118)
+    return '{"Vitamin B6":"'+ref_chromo+'"}'
+
 @application.route('/', methods=['GET'])
 def get():
     return '{"Output":"Hello World"}'
-
 
 @application.route('/', methods=['POST'])
 def post():
